058ec76c12c1a5ac8bc5f6c22d1c44c780a1b830
GaRusMar <ruslangaliev94@gmail.com>
2022-03-20 16:35:13 +0300

    new link

--- vakansii/index.php
+++ vakansii/index.php
@@ -7,42 +7,42 @@ include __DIR__ . "/../assets/inc/header.php";
         <div class="container">
             <h1 class="vacancies__title title title--center wow fadeInUp">Вакансии</h1>
             <div class="vacancies__list">
-                <a href="/vakansii/dizayner-intererov-g-moskva" class="item wow fadeInLeft" data-wow-delay="0s">
+                <a href="/vakansii/dizayner-intererov-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0s">
                     <div class="item__department">В офис</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Дизайнер интерьеров (г. Москва)</h2>
                         <div class="item__arrow"></div>
                     </div>
                 </a>
-                <a href="/vakansii/prorab-g-moskva" class="item wow fadeInLeft" data-wow-delay="0.1s">
+                <a href="/vakansii/prorab-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0.1s">
                     <div class="item__department">Отдел производства</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Прораб (г. Москва)</h2>
                         <div class="item__arrow"></div>
                     </div>
                 </a>
-                <a href="/vakansii/plitochnik-santehnik-g-moskva" class="item wow fadeInLeft" data-wow-delay="0.2s">
+                <a href="/vakansii/plitochnik-santehnik-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0.2s">
                     <div class="item__department">Отдел производства</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Плиточник-сантехник (г. Москва)</h2>
                         <div class="item__arrow"></div>
                     </div>
                 </a>
-                <a href="/vakansii/malyar-shtukatur-g-moskva" class="item wow fadeInLeft" data-wow-delay="0.3s">
+                <a href="/vakansii/malyar-shtukatur-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0.3s">
                     <div class="item__department">Отдел производства</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Маляр-штукатур (г. Москва)</h2>
                         <div class="item__arrow"></div>
                     </div>
                 </a>
-                <a href="/vakansii/elektrik-g-moskva" class="item wow fadeInLeft" data-wow-delay="0.4s">
+                <a href="/vakansii/elektrik-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0.4s">
                     <div class="item__department">Отдел производства</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Электрик (г. Москва)</h2>
                         <div class="item__arrow"></div>
                     </div>
                 </a>
-                <a href="/vakansii/master-po-remontu-kvartir-g-moskva" class="item wow fadeInLeft" data-wow-delay="0.5s">
+                <a href="/vakansii/master-po-remontu-kvartir-g-moskva/" class="item wow fadeInLeft" data-wow-delay="0.5s">
                     <div class="item__department">Отдел производства</div>
                     <div class="item__wrap">
                         <h2 class="item__job">Мастер по ремонту квартир (г. Москва)</h2>
