f2760fa11bd37c0d24d1b8c3b9b685ad535997e8
GaRusMar <ruslangaliev94@gmail.com>
2022-03-20 16:41:22 +0300

    new link

--- uslugi/index.php
+++ uslugi/index.php
@@ -9,33 +9,33 @@ include __DIR__ . "/../assets/inc/header.php";
                 Услуги
             </h1>
             <div class="services-list__list">
-                <a href="/planirovochnye-resheniya" class="item item--hover-dark-green wow fadeInUp">
+                <a href="/planirovochnye-resheniya/" class="item item--hover-dark-green wow fadeInUp">
                     <h2 class="item__title">планировочные решения</h2>
                     <div class="item__description">
                         План с расстановкой мебели и сантехнического оборудования, разработанный с учетом ваших пожеланий и потребностей.
                     </div>
                     <div class="item__price">от 200 руб./кв.м.</div>
                 </a>
-                <a href="/dizayn-proekt-vizualizaciya" class="item item--hover-blue wow fadeInUp">
+                <a href="/dizayn-proekt-vizualizaciya/" class="item item--hover-blue wow fadeInUp">
                     <h2 class="item__title">визуализация</h2>
                     <div class="item__description">Визуальное представление вашего будущего интерьера.</div>
                     <div class="item__price">от 200 руб./кв.м.</div>
                 </a>
-                <a href="/chertezhi" class="item item--hover-purple wow fadeInUp">
+                <a href="/chertezhi/" class="item item--hover-purple wow fadeInUp">
                     <h2 class="item__title">чертежи</h2>
                     <div class="item__description">
                         Чертежная документация отражает пошагово, как нужно проводить ремонтные работы по конкретному проекту.
                     </div>
                     <div class="item__price">от 1000 руб./кв.м.</div>
                 </a>
-                <a href="/polnyy-proekt" class="item item--hover-red wow fadeInUp">
+                <a href="/polnyy-proekt/" class="item item--hover-red wow fadeInUp">
                     <h2 class="item__title">полный проект</h2>
                     <div class="item__description">
                         Включает все проекты (визуализация; чертежи) и контроль реализации проекта дизайнером.
                     </div>
                     <div class="item__price">от 2200 руб./кв.м.</div>
                 </a>
-                <a href="/dopolnitelnye-uslugi" class="item item--hover-green wow fadeInUp">
+                <a href="/dopolnitelnye-uslugi/" class="item item--hover-green wow fadeInUp">
                     <h2 class="item__title">дополнительные услуги</h2>
                     <div class="item__description">
                         Если Вам требуется какая либо отдельная услуга, для нас это не проблема. Если вы не нашли в списке, вы можете позвонить нам и уточнить возможность оказать услугу которая требуется вам
